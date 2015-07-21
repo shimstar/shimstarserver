8a1738285787f0bc6188437aa395813060f1fed8
thyshimrod <shimrod@free.fr>
2015-07-21 21:23:28 +0200

    ajout checkconsistency dans mainserver

--- main.py
+++ main.py
@@ -1,35 +1,40 @@
-from pandac.PandaModules import loadPrcFileData 
+from pandac.PandaModules import loadPrcFileData
+
 loadPrcFileData("", "window-type none")
-#~ loadPrcFileData('','want-pstats 1')
+# ~ loadPrcFileData('','want-pstats 1')
 import direct.directbase.DirectStart
-from pandac.PandaModules import * 
+from pandac.PandaModules import *
 from direct.showbase.DirectObject import DirectObject
 from direct.task.Task import Task
 
 from shimstar.mainserver.network.networktcpserver import *
 from shimstar.mainserver.mainserver import *
 from shimstar.core.stateserver import *
+from shimstar.bdd.checkconsistency import *
+
 
 class ShimStarServer():
-	def __init__(self):
-		NetworkTCPServer()
-		#~ MainServer()
-		taskMgr.add(self.event,"Poll main event",-36)
-		
-	def quit(self):
-		taskMgr.remove("Poll main event")
-		sys.exit()
-		
-	def event(self,task):
-		if StateServer.getInstance().getState()==C_SERVERSTATE_QUIT:
-			self.quit()
-			return Task.done
-		return Task.cont
-	
-	def saveOnForcedQuit(self):
-		print "main::saveOnForcedQuit"
-	 
-app=ShimStarServer()
+    def __init__(self):
+        CheckConsistency().run()
+        NetworkTCPServer()
+        #~ MainServer()
+        taskMgr.add(self.event, "Poll main event", -36)
+
+    def quit(self):
+        taskMgr.remove("Poll main event")
+        sys.exit()
+
+    def event(self, task):
+        if StateServer.getInstance().getState() == C_SERVERSTATE_QUIT:
+            self.quit()
+            return Task.done
+        return Task.cont
+
+    def saveOnForcedQuit(self):
+        print "main::saveOnForcedQuit"
+
+
+app = ShimStarServer()
 run()
-			
+
 atexit.register(app.saveOnForcedQuit)
